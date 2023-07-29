0d1bd1dc1c0b1e15b06e2bd0345ea10170a74bdc
AsifDevopsEngineer <120320386+AsifDevopsEngineer@users.noreply.github.com>
2023-07-29 08:30:44 +0530

    Create EBS_gp2_gp3.py

---
+++ AMINA FATIMA PROJECT.WEBAPP/EBS_gp2_gp3.py
@@ -0,0 +1,18 @@
+import boto3
+
+def lambda_ebs(volume_arn):
+    arn_parts = volume_arn.split(':')
+    volume_id = arn_parts[-1].split('/')[-1]
+    # TODO implement
+    return volume_id
+   
+def lambda_handler(event, context):
+   volume_arn = event['resources'][0]
+   volume_id = lambda_ebs(volume_arn)
+   
+   client = boto3.client('ec2')
+   
+   response = client.modify_volume(
+    VolumeId=volume_id,
+    VolumeType='gp3',
+)
