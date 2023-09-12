ffe066a182c508b9da171a0555109a7b858f6a56
AsifDevopsEngineer <120320386+AsifDevopsEngineer@users.noreply.github.com>
2023-09-12 19:51:43 +0530

    Update EBS_gp2_gp3.py
    commit by AF

--- AMINA FATIMA PROJECT.WEBAPP/EBS_gp2_gp3.py
+++ AMINA FATIMA PROJECT.WEBAPP/EBS_gp2_gp3.py
@@ -3,6 +3,7 @@ import boto3
 def lambda_ebs(volume_arn):
     arn_parts = volume_arn.split(':')
     volume_id = arn_parts[-1].split('/')[-1]
+    #volume_id = volume_arn.split('/')[1] #one time split this is written AF
     # TODO implement
     return volume_id
    
